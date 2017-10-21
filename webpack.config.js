d118a3cf6f79cf9074080fd5f927d2c49600ce11
TJ Santos <tjscode@outlook.com>
2017-10-20 23:07:42 -0700

    change indent to 2 spaces

--- webpack.config.js
+++ webpack.config.js
@@ -4,55 +4,55 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: {
-        polyfills: './app/src/polyfills.js',
-        main: './app/src/main.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'app/dist'),
-        filename: '[name].bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env', 'react']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    { loader: 'css-loader', options: { importLoaders: 1 } },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [
-                                require('postcss-flexbugs-fixes'),
-                                require('autoprefixer')
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
+  entry: {
+    polyfills: './app/src/polyfills.js',
+    main: './app/src/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'app/dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {loader: 'css-loader', options: {importLoaders: 1}},
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                require('autoprefixer')
+              ]
             }
+          }
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['./app/dist']),
-        new HtmlWebpackPlugin({
-            template: './app/src/index.html',
-            inject: false
-        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
     ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['./app/dist']),
+    new HtmlWebpackPlugin({
+      template: './app/src/index.html',
+      inject: false
+    })
+  ]
 };
