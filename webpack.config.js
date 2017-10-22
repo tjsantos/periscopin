d4aa7de56939b7ed56b450a52f3ef231d2f03750
TJ Santos <tjscode@outlook.com>
2017-10-22 15:51:47 -0700

    hashed filenames for caching, separate dev and prod webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
+const NODE_ENV = process.env.NODE_ENV;
+
+const common = {
   entry: {
     polyfills: './app/src/polyfills.js',
     main: ['react-hot-loader/patch', './app/src/main.js']
   },
   output: {
     path: path.resolve(__dirname, 'app/dist'),
-    filename: '[name].bundle.js'
   },
   module: {
     rules: [
@@ -55,9 +56,43 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['./app/dist']),
     new HtmlWebpackPlugin({
-      template: './app/src/index.html',
-      inject: false
+      template: './app/src/index.ejs',
+      excludeChunks: ['polyfills'] // inlined in template to conditionally load polyfills
     }),
-    new webpack.NamedModulesPlugin()
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(NODE_ENV)
+      }
+    })
   ]
 };
+
+const dev = Object.assign({}, common, {
+  output: {
+    ...common.output,
+    filename: '[name].js'
+  },
+  plugins: [
+    ...common.plugins,
+    new webpack.NamedModulesPlugin()
+  ],
+  devtool: 'inline-source-map'
+});
+const prod = Object.assign({}, common, {
+  output: {
+    ...common.output,
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    ...common.plugins,
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.HashedModuleIdsPlugin()
+  ],
+  devtool: 'source-map'
+});
+
+if (NODE_ENV === 'development') {
+  exports.default = dev;
+} else if (NODE_ENV === 'production') {
+  exports.default = prod;
+}
