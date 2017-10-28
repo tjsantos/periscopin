4b81c4da845f8b598738d36f4e911bd45c1b4775
TJ Santos <tjscode@outlook.com>
2017-10-28 03:36:57 -0700

    styling for stream listing

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 // interactive testing using mocks
 // import { mockIO } from '../tests/mockSocket';
@@ -8,23 +9,26 @@ import './App.css';
 const ListItem = () => {
   return (
     <a href="#"
-       className="list-group-item list-group-item-action flex-column align-items-start">
-      <div className="d-flex w-100 justify-content-between">
-        <h5 className="mb-1">List group item heading</h5>
-        <small>3 days ago</small>
+       className="list-group-item list-group-item-action StreamItem">
+      <div
+        className="StreamThumb"
+        style={{backgroundImage: "url(http://via.placeholder.com/350x150)"}}
+      />
+      <div className="StreamDetails">
+        <p className="m-0"><small className="text-muted align-top">3 days ago</small></p>
+        <p className="m-0 overflow-hidden">
+          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget
+          risus varius blandit.
+        </p>
+        <p className="m-0"><small className="text-muted align-bottom">Houston, TX, United States</small></p>
       </div>
-      <p className="mb-1">
-        Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget
-        risus varius blandit.
-      </p>
-      <small>Donec id elit non mi porta.</small>
     </a>
   );
 };
 
 const RefreshButton = () => {
   return (
-    <button type="button" className="btn btn-default btn-lg btn-block" id="refresh-button">
+    <button type="button" className="btn btn-light btn-lg btn-block RefreshButton">
       0 new streams
     </button>
   );
@@ -48,14 +52,12 @@ const GoogleMap = () => {
 };
 
 const App = () => {
-  console.log('rendering App 1');
-
   return (
-    <div className="row no-gutters App">
-      <div className="col-sm-9 order-sm-2 MapContainer">
+    <div className="AppContainer">
+      <div className="MapContainer">
         <GoogleMap/>
       </div>
-      <div className="col-sm-3 order-sm-1 StreamsContainer">
+      <div className="StreamsContainer">
         <Streams/>
       </div>
     </div>
