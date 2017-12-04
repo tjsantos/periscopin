abd16b2f6484c64f2233968c5b4a3b08e4f02cc7
TJ Santos <tjscode@outlook.com>
2017-12-03 22:44:08 -0800

    secret key

--- app/settings.py
+++ app/settings.py
@@ -4,7 +4,10 @@ TWITTER_CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
 TWITTER_CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
 TWITTER_OAUTH_TOKEN = os.environ['TWITTER_OAUTH_TOKEN']
 TWITTER_OAUTH_SECRET = os.environ['TWITTER_OAUTH_SECRET']
-DEBUG = os.environ.get('APP_DEBUG', False)
-CAPTURE_DATA = os.environ.get('APP_CAPTURE_DATA', False)
+DEBUG = os.environ.get('APP_DEBUG')
+APP_DATA_MODE = os.environ.get('APP_DATA_MODE', 'default')  # 'capture', 'replay', or default
 PORT = int(os.environ.get('PORT', 5000))
-SECRET_KEY = 'secret!'  # no encryption/sessions used
+try:
+    SECRET_KEY = os.environ['APP_SECRET_KEY']
+except KeyError:
+    raise Exception('secret key is not set')
